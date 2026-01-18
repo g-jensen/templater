bcb39991e3d60ff0af95f06162191a94f5addd25
greg <greg.micah.jensen@gmail.com>
2026-01-18 09:48:08 -0500

    adds clarifying test....

--- internal/template/list_features_test.go
+++ internal/template/list_features_test.go
@@ -3,9 +3,10 @@ package template
 import (
 	"testing"
 
+	"templater/internal/testutil/fs"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"templater/internal/testutil/fs"
 )
 
 func TestListFeatures(t *testing.T) {
@@ -36,4 +37,22 @@ func TestListFeatures_ExcludesRootBasePatch(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Empty(t, features)
+}
+
+func TestListFeatures_MissingBasePatch(t *testing.T) {
+	memfs := fs.NewMemoryFS()
+	memfs.AddDir("repo")
+	memfs.AddDir("repo/auth")
+	memfs.AddDir("repo/auth/oauth")
+	memfs.AddDir("repo/auth/oauth/google")
+	memfs.AddDir("repo/auth/oauth/github")
+	memfs.AddFile("repo/auth/base.patch", []byte{})
+	memfs.AddFile("repo/auth/oauth/google/base.patch", []byte{})
+	memfs.AddFile("repo/auth/oauth/github/base.patch", []byte{})
+
+	features, err := ListFeatures(memfs, "repo")
+	require.NoError(t, err)
+
+	expected := []string{"auth", "auth/oauth/github", "auth/oauth/google"}
+	assert.Equal(t, expected, features)
 }
